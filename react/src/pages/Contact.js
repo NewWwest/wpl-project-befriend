77bebda32ef2f074666ea5987aa0aa445ac4382c
Andrzej Westfalewicz <awestfal@gmail.com>
2022-04-19 09:42:10 +0200

    Set the correct repository in urls

--- react/src/pages/Contact.js
+++ react/src/pages/Contact.js
@@ -3,7 +3,7 @@ const Contact = () => {
     <div>
       <h2>About</h2>
       <p>When hosting this project make sure you include your contact information here</p>
-      <p>You can check out the code of this project here:  <a href="https://github.com/NewWwest/wpl-befriend">https://github.com/NewWwest/wpl-befriend</a></p>
+      <p>You can check out the code of this project here:  <a href="https://github.com/NewWwest/wpl-project-befriend">https://github.com/NewWwest/wpl-project-befriend</a></p>
     </div>
     );
     
